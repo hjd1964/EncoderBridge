97a27774cc8b2f485bd8ab635a724a2cbe0b5ad9
Howard Dutton <hjd1964@gmail.com>
2025-01-09 08:51:12 -0500

    Better formatting and organization

--- src/Config.defaults.h
+++ src/Config.defaults.h
@@ -2,7 +2,17 @@
 // Configuration defaults
 #pragma once
 
+// host name for this microcontroller
+#ifndef HOST_NAME
+#define HOST_NAME                      "OnStep-EB"
+#endif
+
+// settings identification
+#ifndef CONFIG_NAME
+#define CONFIG_NAME                    HOST_NAME
+#endif
+
 // use the HAL specified default NV driver
 #ifndef NV_DRIVER
-#define NV_DRIVER                     NV_DEFAULT
+#define NV_DRIVER                      NV_DEFAULT
 #endif
